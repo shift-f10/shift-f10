0040423399137137a56b8065cfbe2d310913e72c
shift-f10 <40450442+shift-f10@users.noreply.github.com>
2018-06-20 19:25:19 -1000

    Initial upload

---
+++ Gen-Mapper.ps1
@@ -0,0 +1,74 @@
+﻿Function Gen-Mapper {
+    
+
+
+    Write-Host "1. Open your modern browser and navigate to http://jsfiddle.net" -ForegroundColor Gray
+    Write-Host "`n"
+    Write-Host "2. Copy the code below and paste into the HTML window." -ForegroundColor Gray
+    Write-Host `<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> 
+    Write-Host `<div id="chart_div"></div> 
+    Write-Host "`n" 
+    Write-Host "3. Copy the code below and pase into the Javascript window." -ForegroundColor Gray
+    Write-Host "`n"
+    Write-Host "google.charts.load('current', {packages:[`"orgchart`"]});"
+    Write-Host "google.charts.setOnLoadCallback(drawChart);"
+    Write-Host "function drawChart() {"
+    Write-Host "var data = new google.visualization.DataTable();"
+    Write-Host "data.addColumn('string', 'PID');"
+    Write-Host "data.addColumn('string', 'PPID');"
+    Write-Host "data.addColumn('string', 'Information');"
+    Write-Host "data.addRows(["
+    
+    # Change or remove the MaxEvents value to the number of items you want to see
+    # or change the FilterXPath to a more defined filter
+    $logs = Get-WinEvent -LogName Security -FilterXPath "*[System[(EventID=4688)]]" -MaxEvents 100
+    foreach ($entries in $logs) {
+
+        #return PID
+        $getpid = $entries.message -split "\r\n" | Select-String -SimpleMatch "New Process ID:"
+        [array]$hexPID = $getpid -split ":"
+        $ProcID = [System.Convert]::ToInt64($hexPID[1].trim(),16)
+
+        #return Process Name
+        $getproc = $entries.message -split "\r\n" | Select-String -SimpleMatch "New Process Name:"
+        [array]$arrProc = $getproc -split "\t"
+        $ProcName = $arrProc[2]
+
+        #return Parent PID
+        $getppid = $entries.message -split "\r\n" | Select-String -SimpleMatch "Creator Process ID:"
+        [array]$hexPPID = $getppid -split ":"
+        $PPID = [System.Convert]::ToInt64($hexPPID[1].trim(),16)
+
+        #return Parent Process Name
+        $getpproc = $entries.message -split "\r\n" | Select-String -SimpleMatch "Creator Process Name:"
+        [array]$arrPProc = $getpproc -split "\t"
+        $PProcName = $arrPProc[2]
+
+        #return Account Name
+        $getAcctinfo = $entries.message -split "\r\n" | Select-String -SimpleMatch "Account Name:"
+        [array]$arrAcct = $getAcctinfo -split ":"
+        $Account = $arrAcct[1].trim()
+
+        #return Domain
+        $getDomain = $entries.message -split "\r\n" | Select-String -SimpleMatch "Account Domain:"
+        [array]$arrDomain = $getDomain -split ":"
+        $Domain = $arrDomain[1].trim()
+
+        $PName = [regex]::Escape($ProcName) + ", " + [regex]::Escape($Account)
+
+        Write-Host "['$ProcID', '$PPID', '$PName'],"
+
+    } 
+    Write-Host "['EventLog Mapper','','Charting Process 4688 history from eventlog.']]);"
+    Write-Host "var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));"
+    Write-Host "chart.draw(data, {allowHtml:true});}"
+    Write-Host "`n"
+    Write-Host "4. Click the Run button to generate the map!" -ForegroundColor Gray
+     
+}    
+
+Gen-Mapper 
+
+
+
+
---
+++ Get-psEventLog.ps1
@@ -0,0 +1,55 @@
+﻿Function Get-psEventLog {
+    
+    
+    # Change or remove the MaxEvents value to the number of items you want to see
+    # or change the FilterXPath to a more defined filter
+    $logs = Get-WinEvent -LogName Security -FilterXPath "*[System[(EventID=4688)]]" -MaxEvents 25
+    
+    foreach ($entries in $logs) {
+
+        #return PID
+        $getpid = $entries.message -split "\r\n" | Select-String -SimpleMatch "New Process ID:"
+        [array]$hexPID = $getpid -split ":"
+        $ProcID = [System.Convert]::ToInt64($hexPID[1].trim(),16)
+
+        #return Process Name
+        $getproc = $entries.message -split "\r\n" | Select-String -SimpleMatch "New Process Name:"
+        [array]$arrProc = $getproc -split "\t"
+        $ProcName = $arrProc[2]
+
+        #return Parent PID
+        $getppid = $entries.message -split "\r\n" | Select-String -SimpleMatch "Creator Process ID:"
+        [array]$hexPPID = $getppid -split ":"
+        $PPID = [System.Convert]::ToInt64($hexPPID[1].trim(),16)
+
+        #return Parent Process Name
+        $getpproc = $entries.message -split "\r\n" | Select-String -SimpleMatch "Creator Process Name:"
+        [array]$arrPProc = $getpproc -split "\t"
+        $PProcName = $arrPProc[2]
+
+        #return Account Name
+        $getAcctinfo = $entries.message -split "\r\n" | Select-String -SimpleMatch "Account Name:"
+        [array]$arrAcct = $getAcctinfo -split ":"
+        $Account = $arrAcct[1].trim()
+
+        #return Domain
+        $getDomain = $entries.message -split "\r\n" | Select-String -SimpleMatch "Account Domain:"
+        [array]$arrDomain = $getDomain -split ":"
+        $Domain = $arrDomain[1].trim()
+
+
+        $out = New-Object -TypeName psobject
+        $out | Add-Member -MemberType NoteProperty -Name Timestamp -Value $entries.TimeCreated
+        $out | Add-Member -MemberType NoteProperty -Name PID -Value $ProcID
+        $out | Add-Member -MemberType NoteProperty -Name Process -Value $ProcName
+        $out | Add-Member -MemberType NoteProperty -Name Domain -Value $Domain
+        $out | Add-Member -MemberType NoteProperty -Name User -Value $Account            
+        $out | Add-Member -MemberType NoteProperty -Name PPID -Value $PPID
+        $out | Add-Member -MemberType NoteProperty -Name ParentProcess -Value $PProcName
+        $out
+
+    } 
+}    
+
+# Pipe the output to any format you like or view
+Get-psEventLog | Out-GridView
